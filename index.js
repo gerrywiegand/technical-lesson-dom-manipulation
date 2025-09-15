6f04cd02d9d1b3b96a71fde8fbd4363621b7cf38
Gerry <gerrywiegand@gmail.com>
2025-09-15 09:19:09 -0400

    declare var

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // Write your code here!
+const userInfo = {
+  username: "FlatUser123",
+  description: "Hi, welcome to my webpage! Just a new user of this website!",
+  theme: "red",
+};
 
+function displayUser(userInfo) {}
+
+displayUser(userInfo);
